73c2591b55d8bcf3f6ed2441102729f321c49af9
Anastasiia Shapoval <nastya030207@gmail.com>
2026-03-14 12:01:56 +0200

    feat: add ValidationException throwable

---
+++ src/main/java/ua/university/order/exception/ValidationException.java
@@ -0,0 +1,5 @@
+package ua.university.order.exception;
+
+public class ValidationException extends AppException{
+    public ValidationException(String message, Throwable cause) { super(message, cause); }
+}
