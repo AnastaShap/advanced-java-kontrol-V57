87320202db94c743ae2b6b51321425eca52c740e
Anastasiia Shapoval <nastya030207@gmail.com>
2026-03-14 10:57:07 +0200

    feat: finalise Order and OrderItem

--- src/main/java/ua/university/order/domain/Order.java
+++ src/main/java/ua/university/order/domain/Order.java
@@ -1,9 +1,17 @@
 package ua.university.order.domain;
 
+import java.util.Objects;
+
+// Main Entity: Order (use defensive copy)
 public class Order {
     private final String id;
-    private final OrderItem[] items;
+    private final OrderItem[] items; //array of items
     private OrderStatus status;
+
+    // Ланцюжок конструкторів (this)
+    public Order(String id) {
+        this(id, new OrderItem[0]);
+    }
 
     public Order(String id, OrderItem[] items) {
         this.id = id;
@@ -11,14 +19,29 @@ public class Order {
         this.status = OrderStatus.NEW;
     }
 
-    /*public OrderItem[] getItems() {
-        return items.clone(); // Повертаємо копію
-    }*/
+    public String getId() {
+        return id;
+    }
+    public OrderItem[] getItems() {
+        return items.clone();
+
+    }
+    public OrderStatus getStatus() {
+        return status;
+    }
+    public void setStatus(OrderStatus status) {this.status = status;}
 
     @Override
-    public String toString() {
-        return super.toString();
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
--- src/main/java/ua/university/order/domain/OrderItem.java
+++ src/main/java/ua/university/order/domain/OrderItem.java
@@ -1,4 +1,30 @@
 package ua.university.order.domain;
 
-public enum OrderItem {
+public class OrderItem {
+    private final String productName;
+    private final String category;
+    private final int quantity;
+    private final Money price;
+
+    public OrderItem(String productName, String category, int quantity, Money price) {
+        this.productName = productName;
+        this.category = category;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Money total() {
+        return price.multiply(quantity);
+    }
+    @Override
+    public String toString() { return "Item{name='" + productName + "', price=" + price + "}"; }
+
 }
