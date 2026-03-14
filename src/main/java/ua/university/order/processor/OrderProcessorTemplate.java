4bf852fed38135e9c4003a9c560dfb28a42c4306
Anastasiia Shapoval <nastya030207@gmail.com>
2026-03-14 12:02:57 +0200

    refactor: add try catch and logging

--- src/main/java/ua/university/order/processor/OrderProcessorTemplate.java
+++ src/main/java/ua/university/order/processor/OrderProcessorTemplate.java
@@ -4,22 +4,38 @@ import ua.university.order.domain.Money;
 import ua.university.order.domain.Order;
 import ua.university.order.domain.OrderItem;
 import ua.university.order.domain.OrderStatus;
+import ua.university.order.exception.AppException;
+import ua.university.order.exception.ValidationException;
 import ua.university.order.payment.PaymentMethod;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public abstract class OrderProcessorTemplate {
 
+    protected static final Logger logger = Logger.getLogger(OrderProcessorTemplate.class.getName());
+
+    // Шаблонний метод (final)
+    public final void process(Order order) throws AppException {
+
+    }
+
     public final void process(Order order, PaymentMethod paymentMethod) {
 
-        validate(order);
-        validateCategoryMix(order);
+        try{
+            validate(order);
+            validateCategoryMix(order);
+            Money total = calculate(order);
+            paymentMethod.pay(total);
+            finish(order);
 
-        Money total = calculate(order);
-
-        paymentMethod.pay(total);
-
-        finish(order);
+        } catch (AppException e) {
+        logger.warning("Business error: " + e.getMessage());
+        throw e;
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Infrastructure failure", e);
+            throw new ValidationException("Critical failure", e); // Chaining
+        }
     }
 
     protected abstract void validate(Order order);
